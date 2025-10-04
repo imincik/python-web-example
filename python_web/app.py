bd57b302e930f3b8b80448d2c08a3aac7d48e4ec
Ivan Mincik <ivan.mincik@gmail.com>
2025-10-04 21:11:54 +0200

    Set default db and username to postgres

--- python_web/app.py
+++ python_web/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 # Database connection settings (use environment variables for flexibility)
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "mydb")
-DB_USER = os.getenv("DB_USER", "myuser")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "mypassword")
 
 
